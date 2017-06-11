e75e805ccdf62ff4ff8a9b04dafb055667186dd5
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2017-06-11 22:17:25 +0200

    Create HelloWorld.java

--- billStrategy/HelloWorld.java
+++ billStrategy/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String []args){
          
-         
+         //with strategy design pattern you can class can be changed at run time
            Shop shopWithoutDiscount=new Shop(new Regular());
            Shop shopWithDiscount=new Shop(new Discount());
          
