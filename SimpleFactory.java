2342501da08d8f75ab37981aa1f6b05793ff3709
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2017-11-01 01:25:16 +0200

    Create SimpleFactory.java

---
+++ SimpleFactory.java
@@ -0,0 +1,58 @@
+
+//factory method and simple factory
+public class PizzaStore {
+    public Pizza orderPizza(String name) {
+        Pizza pizza ;
+        // but this part will alwayse change
+        //changable behaviour must be encapsulated
+        if (name == 'tona') {
+            pizza = new tona();
+        } else if (name == 'chees') {
+            pizza= new chees();
+        }
+        else if (name == 'meat') {
+            pizza= new meat();
+        }
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza
+    }
+}
+////////////////////////////////////////////
+//encapsulation object creation
+public class SimplePizzaFactory {
+    public Pizza createPizza(String name) 
+    {
+        if (name == 'tona') {
+            return new tona();
+        } else if (name == 'chees') {
+            return  new chees();
+        }
+        else if (name == 'meat') {
+            return new meat();
+        }
+
+        return null;
+    }
+}
+public class PizzaStore {
+    SimplePizzaFactory factory;
+    public PizzaStore(SimplePizzaFactory factory){
+    this.factory = factory;
+    }
+    public Pizza orderPizza(String name) {
+       
+        Pizza pizza =this.factory.createPizza(name);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza
+    }
+}
