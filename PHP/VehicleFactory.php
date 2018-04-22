6b8238626bc6a1c4796ac00922eec3a3c45e37bd
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2018-04-22 09:19:55 +0200

    adding factory DP

---
+++ PHP/VehicleFactory.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\DesignPatterns;
+class VehicleFactory
+{
+    public static function build ($type) 
+    {
+             
+        if(empty($type)) {
+            throw new Exception('Invalid Vehicle Type.');
+        } else {
+ 
+            $className = ucfirst($type);
+
+            if(class_exists($className)) {
+                return new $className();
+            } else {
+                throw new \Exception('Vehicle type not found.');
+            }
+        }
+    }
+}
+
+ class Vehicle {
+    public $factory;
+    public function __construct()
+    {
+        $this->factory = new VehicleFactory;
+    }
+    public function doStuff($vehicleName) 
+    {
+        $vehicleObject = $this->factory::build($vehicleName); 
+       //// ... do stuff
+    }
+}
+ 
+class Van 
+{
+     
+    public function __construct() 
+    {
+        echo "Creating Van";
+    }
+   
+     
+}
+ 
+class Truck 
+{
+ 
+    public function __construct() 
+    {
+        echo "Creating Truck";
+    }
+ 
+}
+ 
+class Bus 
+{
+ 
+    public function __construct() 
+    {
+        
+        echo "Creating Bus";
+    }
+   
+}
+
+    
